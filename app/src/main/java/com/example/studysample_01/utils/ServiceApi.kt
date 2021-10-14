56942c2312593fc62f48f6a1a79b93285bb52319
DR-kimMinJin <kmj@drdiay.co.kr>
2021-10-14 13:30:26 +0900

    test push2

--- app/src/main/java/com/example/studysample_01/utils/ServiceApi.kt
+++ app/src/main/java/com/example/studysample_01/utils/ServiceApi.kt
@@ -5,7 +5,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ServiceApi {
-
+    // branch 2 test
     @GET(API.SEARCH_BOOK)
     suspend fun getSearchBook(
         @Query("query") query: String
